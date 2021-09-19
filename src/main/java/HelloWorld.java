563672106149b151fd1e9f89ad5971f6e09d0268
varshaghanate <90283894+varshaghanate@users.noreply.github.com>
2021-09-19 23:40:48 +0100

    eclipse changes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
         System.out.println("this is my commit from feature into master");
         System.out.println("this demo for no conflict");
         System.out.println("This demo");
-        System.out.println("this demo is for git rebase from intellj"); 
+        System.out.println("this demo is for git rebase from ecllipse"); 
        
     }
     void Hello(){
