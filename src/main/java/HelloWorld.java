3dd9d8e99769e8c52a674c229b789f1a83b67052
varshaghanate <90283894+varshaghanate@users.noreply.github.com>
2021-09-19 22:34:30 +0100

    Demo fo git rebase

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("this is my first github commit integrating with jenkins");
         System.out.println("this is my commit from feature into master");
+        System.out.println("this demo for no conflict");
+        System.out.println("This demo");
+        System.out.println("this demo is for git rebase from intellj"); 
+       
+    }
+    void Hello(){
     }
 }
